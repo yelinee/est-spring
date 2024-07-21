fc1dda05c9df6a841eb89cc7df2008ccbb495d00
yelinee <ami573@naver.com>
2024-07-21 22:54:55 +0900

    init

---
+++ src/test/java/com/example/weeklyquiz4/WeeklyQuiz4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weeklyquiz4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyQuiz4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
