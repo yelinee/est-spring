a97b4d9cbd2e6fa6b3ab4136da5f10ac65335ac3
yelinee <ami573@naver.com>
2024-07-24 23:51:58 +0900

    메뉴 관리 기능 추가

--- src/main/java/com/example/weeklyquiz4/Repository/MenuRepository.java
+++ src/main/java/com/example/weeklyquiz4/Repository/MenuRepository.java
@@ -1,4 +1,15 @@
-package com.example.weeklyquiz4.Repository;
+package com.example.weeklyquiz4.repository;
 
-public class MenuRepository {
+import com.example.weeklyquiz4.entity.MenuEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+
+public interface MenuRepository extends JpaRepository<MenuEntity, Long> {
+
+    // 카테고리로 메뉴 찾기
+    @Query("select m from MenuEntity m where m.menuCategory = ?1")
+    List<MenuEntity> findByMenuCategory(String menuCategory);
 }
